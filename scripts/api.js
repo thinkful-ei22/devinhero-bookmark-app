680bbddada7e931ea722bb388e1add9601da5c91
dghero <devingreyhero@gmail.com>
2018-06-21 14:33:01 -0500

    Add api functions for get,create,delete

--- scripts/api.js
+++ scripts/api.js
@@ -1,3 +1,5 @@
+/* global cuid */
+
 'use strict';
 
 console.log('api.js');
@@ -6,13 +8,41 @@ console.log('api.js');
 const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/devinhero/bookmarks';
 
-  //getItems()
+  const getBookmarks = function(callback){
+    $.getJSON(`${BASE_URL}`, callback);
+  };
 
-  //createItem()
+  const createBookmark = function(title, url, rating, desc, callback, error){
+    const newBookmark = JSON.stringify({
+      title, url, rating, desc
+    });
+
+    $.ajax({
+      url: `${BASE_URL}`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newBookmark,
+      success: callback,
+      error: error
+    });
+  };
+
+  const deleteBookmark = function(id, callback, error){
+    $.ajax({
+      url: `${BASE_URL}/${id}`,
+      method: 'DELETE',
+      contentType: 'application/json',
+      success: callback,
+      error: error
+    });
+  };
 
   //updateItem() //Stretch goal
-
-  //deleteItem()
+  return {
+    getBookmarks,
+    createBookmark,
+    deleteBookmark,
+  };
 
 
 }() );
--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,19 @@ console.log('index.js');
 $(document).ready(function() {
   bookmarks.bindEventListeners();
   
+  api.getBookmarks(function(response){
+    console.log('Get Items initial: ', response);
+  });
+
+  
+});
+  
+  // api.createBookmark('METAL, But Not', 'https://www.youtube.com/watch?v=dndsJunKgaY', 3, 'wow', function(response){
+
+  // }, function(response){
+  //   console.log('Create failed: ', response.responseJSON.message);
+  // });
+
   // api.getItems((items) => {
   //   items.forEach((item) => store.addItem(item));
   //   shoppingList.render();
